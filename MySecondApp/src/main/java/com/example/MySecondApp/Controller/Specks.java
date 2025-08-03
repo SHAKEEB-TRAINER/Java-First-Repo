f320acdd7851bf848591fd8400523eb50290575f
SHAKEEB-TRAINER <shaiksameer7003@gmail.com>
2025-08-03 22:29:40 +0530

    Add files via upload

---
+++ MySecondApp/src/main/java/com/example/MySecondApp/Controller/Specks.java
@@ -0,0 +1,19 @@
+package com.example.MySecondApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Specks {
+
+    @RequestMapping(value = "/Glasses",method = RequestMethod.GET)
+    public List<String> getAllGlass(){
+
+
+        return List.of("SUN GLASES","MOON GLASES","REBN GLASES");
+    }
+}
